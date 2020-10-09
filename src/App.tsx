529f5ada77a792202265467bb4cf98694223e3c7
고규훈 <kh.ko@airpremia.com>
2020-10-10 00:46:34 +0900

    add 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { SyntheticEvent } from 'react';
+import './App.scss';
+import { Card, Input, List } from 'antd';
+import {
+  PlusCircleFilled,
+  CloseCircleOutlined,
+} from '@ant-design/icons';
+import {
+  useLocalObservable,
+  Observer,
+} from 'mobx-react-lite';
 
 function App() {
+  const todo = useLocalObservable(() => ({
+    inputValue: '',
+    handleChange(e: SyntheticEvent) {
+      const { value } = e.target as HTMLInputElement;
+      this.inputValue = value;
+    },
+    list: [] as string[],
+    insert() {
+      this.list = [...this.list, this.inputValue];
+      this.inputValue = '';
+    },
+    remove() {
+      // console.log(index);
+      // this.list = this.list.splice(index, 1);
+    },
+  }));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Observer>
+      {() => (
+        <div className="App">
+          <Card title="To Do">
+            <Input
+              placeholder="Insert To Do"
+              addonAfter={
+                <PlusCircleFilled onClick={todo.insert} />
+              }
+              value={todo.inputValue}
+              onChange={todo.handleChange}
+              onPressEnter={todo.insert}
+            />
+            <List
+              className="list"
+              bordered
+              dataSource={todo.list}
+              renderItem={item => (
+                <List.Item
+                  actions={[
+                    <CloseCircleOutlined
+                      onClick={todo.remove}
+                    />,
+                  ]}
+                >
+                  {item}
+                </List.Item>
+              )}
+            />
+          </Card>
+        </div>
+      )}
+    </Observer>
   );
 }
 
